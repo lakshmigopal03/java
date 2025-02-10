0cbe6123a0469c8b5ddbde96b8965e921ab96252
lakshmigopal03 <gopallakshmi154@gmail.com>
2025-02-10 14:07:02 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();  
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();  
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping:");
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        scanner.close();
+    }
+}
